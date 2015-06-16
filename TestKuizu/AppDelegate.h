06eeefe99ce505097668042b3c7995cb4fde4749
KazuyukiHiga <w-_-waaa1@KazuyukiMac.local>
2015-06-16 23:31:37 +0900

    Initial Commit

---
+++ TestKuizu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestKuizu
+//
+//  Created by 比嘉　和之 on 2015/06/16.
+//  Copyright (c) 2015年 比嘉　和之. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestKuizu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestKuizu
+//
+//  Created by 比嘉　和之 on 2015/06/16.
+//  Copyright (c) 2015年 比嘉　和之. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
